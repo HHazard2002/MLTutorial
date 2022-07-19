08ff635997021ac6b759dfa8575bc8b1bbc095ab
HHazard2002 <hugo7.hazard@gmail.com>
2022-07-19 21:43:21 +0100

    bulding the dense layer

--- convolution.py
+++ convolution.py
@@ -52,3 +52,9 @@ model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.summary()
 
 #depth of the image increases but spacial dimensions reduce drastically
+
+
+#dense layers
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
