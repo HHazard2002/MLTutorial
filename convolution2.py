5b611f9f01d637f502ad845db303f175e4aa60b8
HHazard2002 <hugo7.hazard@gmail.com>
2022-07-30 12:23:18 +0100

    split data

--- convolution2.py
+++ convolution2.py
@@ -7,3 +7,12 @@ keras = tf.keras
 
 import tensorflow_dataset as tfds
 tfds.disable_progress_bar()
+
+#split the data manually into 80% training, 10% testing, 10% validation
+(raw_train, raw_validation, raw_test), metadata = tfds.load(
+  'cats_vs_dog',
+  split=['train[:80%]', 'test[80%:90%]', 'validation[90%:]'],
+  with_info=True,
+  as_supervised=True)
+
+get_label_name = metadata.features['label'].int2str #creates a function object that we can use to get the labels
