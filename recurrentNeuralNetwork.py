3666b20fe3fd863c1120a419b323f36b9f0c3429
HHazard2002 <hugo7.hazard@gmail.com>
2022-08-02 22:12:35 +0100

    bag of words

--- recurrentNeuralNetwork.py
+++ recurrentNeuralNetwork.py
@@ -6,3 +6,25 @@
 # which is stored in a collection which does not keep track of the 
 # order but does for the frequency\
 
+vocab = {}
+word_encoding = 1
+def bag_of_words(text):
+  global word_encoding
+
+  words = text.lower().split(" ") #create a list of all of the words in the text, we do not care about grammar in this case
+  bag = {} #stores all of the encodings and their frequency
+
+  for word in words:
+    if word in vocab:
+      encoding = vocab[word] #get encoding from vocab
+    else:
+      vocab[word] = word_encoding
+      encoding = word_encoding
+      word_encoding += 1
+
+    if encoding in bag:
+      bag[encoding] += 1
+    else:
+      bag[encoding] = 1
+
+  return bag
