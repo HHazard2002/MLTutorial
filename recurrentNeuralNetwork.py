5350128ad4487adfb8f782d73c4e0eb1a586bbf9
HHazard2002 <hugo7.hazard@gmail.com>
2022-08-02 22:12:12 +0100

    commit

---
+++ recurrentNeuralNetwork.py
@@ -0,0 +1,8 @@
+#This focusses on NLP (Natural Language Processing)
+
+#How to encode data?
+#1. Bag of words: 
+# each word in a sentence is encoded with a number
+# which is stored in a collection which does not keep track of the 
+# order but does for the frequency\
+
