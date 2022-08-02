9df09dd211f329e28bfa9d41f6a5d8784570ea82
HHazard2002 <hugo7.hazard@gmail.com>
2022-08-02 22:13:42 +0100

    one hot encoding

--- recurrentNeuralNetwork.py
+++ recurrentNeuralNetwork.py
@@ -40,3 +40,20 @@ print(vocab)
 
 vocab = {}
 word_encoding = 1
+
+def one_hot_encoding(text):
+  global word_encoding
+
+  words = text.lower().split(" ")
+  encoding = []
+
+  for word in words:
+    if word in vocab:
+      code = vocab[word]
+      encoding.append(code)
+    else:
+      vocab[word] = word_encoding
+      encoding.append(word_encoding)
+      word_encoding += 1
+
+  return encoding
