894c85a69428be8e9c627a6101a8d933a1db63ec
HHazard2002 <hugo7.hazard@gmail.com>
2022-08-04 17:58:11 +0100

    encode text

--- recurrentNeuralNetwork.py
+++ recurrentNeuralNetwork.py
@@ -121,3 +121,17 @@ history = model.fit(train_data, train_labels, epochs=10, validation_split=0.2)
 
 results = model.evaluate(test_data, test_labels)
 print(results)
+
+# Making predictions
+# First we need to convert any review into an encoding that the network can understand
+
+word_index = imdb.get_word_index()
+
+def encode_text(text):
+  tokens = keras.preprocessing.text.text_to_word_sequence(text)
+  tokens = [word_index[word] if word in word_index else 0 for word in tokens]
+  return sequence.pad_sequences([tokens], MAXLEN)[0]
+
+text = "that movie was just amazing, so amazing"
+encoded = encode_text(text)
+print(encoded)
