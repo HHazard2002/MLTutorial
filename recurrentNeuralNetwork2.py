bdcc4a781acf884af71c97617366804913ef94a3
HHazard2002 <hugo7.hazard@gmail.com>
2022-08-05 20:23:35 +0100

    read file content

--- recurrentNeuralNetwork2.py
+++ recurrentNeuralNetwork2.py
@@ -12,3 +12,11 @@ path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googl
 
 from google.colab import files
 path_to_file = list(files.upload().keys())[0]
+
+# read content of file
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+# length of text is the number of characters in it
+print('Length of text: {} characters'.format(len(text)))
+
+print(text[:250])
+
