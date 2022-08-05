ffed193b383da905098f33c9d0b5b11b70170bd8
HHazard2002 <hugo7.hazard@gmail.com>
2022-08-05 20:22:52 +0100

    imports

---
+++ recurrentNeuralNetwork2.py
@@ -0,0 +1,9 @@
+# RNN Play Generator
+from pickletools import optimize
+from syslog import LOG_SYSLOG
+from convolutional2 import BATCH_SIZE
+from keras.preprocessing import sequence
+import keras
+import tensorflow as tf
+import os
+import numpy as np
