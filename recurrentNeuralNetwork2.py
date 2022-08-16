ead586ce3941c9723dce8d7987d1ecee2a3fa129
HHazard2002 <hugo7.hazard@gmail.com>
2022-08-16 18:45:49 +0100

    input

--- recurrentNeuralNetwork2.py
+++ recurrentNeuralNetwork2.py
@@ -185,3 +185,5 @@ def generate_text(model, start_string):
     text_generated.append(idx2char[predicted_id])
 
     return (start_string + ''.join(text_generated))
+
+inp = input("Type a starting string: ")
