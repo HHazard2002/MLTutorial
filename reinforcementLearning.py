9910fbca85abde2e8cfc165bd1fe5741e4b4007c
HHazard2002 <hugo7.hazard@gmail.com>
2022-08-18 20:08:40 +0100

    agent

--- reinforcementLearning.py
+++ reinforcementLearning.py
@@ -1,3 +1,7 @@
 # Terminology
 # Environment:
 # This is what the agent will explore (ie. a level in mario)
+
+# Agent:
+# Entity that is exploring the environment. The agent interacts and takes
+# actions within the env (ie. mario)
